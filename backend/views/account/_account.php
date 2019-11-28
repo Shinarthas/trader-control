ec3addb0fe4d8d708cb8121ce22a60c18d78ace3
UniLex6 <unilex6@gmail.com>
2019-11-28 17:17:01 +0200

    tt

--- backend/views/account/_account.php
+++ backend/views/account/_account.php
@@ -3,12 +3,14 @@
 ?>
 
 <div>
+<a href="/account/<?=$account->id;?>">
 <?=$account->id;?> <?=$account->label;?>
 <?
-	$data = ApiRequest::statistics('v1/acc-balance/index', ['account_id'=>$account->id, 'limit'=>2]);
+	$data = ApiRequest::statistics('v1/acc-balance/last', ['account_id'=>$account->id]);
 
 	foreach($data->data as $balance) 
 		echo '<span style="padding:0 20px;">'.$balance->name.' '.$balance->value.'/'.$balance->value_in_orders.'</span>';
 	
 ?>
+</a>
 </div>
