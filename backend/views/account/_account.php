4ee8255bc00fe9ca53c23dc3a7396cac2793358e
UniLex6 <unilex6@gmail.com>
2019-11-28 16:02:13 +0200

    fix

--- backend/views/account/_account.php
+++ backend/views/account/_account.php
@@ -1,6 +1,14 @@
-
+<?
+	use common\components\ApiRequest;
+?>
 
 <div>
 <?=$account->id;?> <?=$account->label;?>
+<?
+	$data = ApiRequest::statistics('v1/acc-balance/index', ['account_id'=>$account->id, 'limit'=>2]);
 
+	foreach($data->data as $balance) 
+		echo '<span style="padding:0 20px;">'.$balance->name.' '.$balance->value.'/'.$balance->value_in_orders.'</span>';
+	
+?>
 </div>
